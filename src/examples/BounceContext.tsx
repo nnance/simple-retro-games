38f7db0e44aa40dcc1896f8c3ed7cdb80379f2c4
Nick Nance <nance.nick@gmail.com>
2020-08-23 22:33:19 -0700

    remove grid from bounce context

--- src/examples/BounceContext.tsx
+++ src/examples/BounceContext.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import {
   IRect,
   idFactory,
@@ -7,56 +7,20 @@ import {
   movementSystem,
   collisionSystem,
   useAnimationFrame,
-  IPoint,
-  ISystem,
 } from "../lib";
-
-const brickSize = { width: 60, height: 20 };
-const rows = 2;
-const cols = 7;
 
 const GameContext = React.createContext<
   [IParticle[], React.Dispatch<React.SetStateAction<IParticle[]>>]
 >([[], () => []]);
 
-const Brick = ({ pos, size }: IParticle) => {
-  return <rect {...pos} {...size} stroke="grey" fill="none" />;
-};
-
-const Grid = ({ x, y }: IPoint) => {
-  const [particles] = React.useContext(GameContext);
-
-  return (
-    <Fragment>
-      {particles.map((brick) =>
-        brick.family === "brick" ? <Brick {...brick} /> : null
-      )}
-    </Fragment>
-  );
-};
-
 const Ball = ({ pos, radius }: IParticle) => {
   return <circle r={radius} cx={pos.x} cy={pos.y} stroke="grey" fill="none" />;
-};
-
-const brickCollisionSystem: ISystem = (world) => {
-  const particles = world.particles.reduce((prev, particle) => {
-    const hit = world.events.find(
-      (_) => _.particle.id === particle.id && particle.family === "brick"
-    );
-    return hit ? prev : [...prev, particle];
-  }, [] as IParticle[]);
-  return { ...world, particles };
 };
 
 const Board = (props: React.PropsWithChildren<IRect>) => {
   const [particles, setParticles] = React.useContext(GameContext);
 
-  const gameLoop = updater([
-    movementSystem,
-    collisionSystem,
-    brickCollisionSystem,
-  ]);
+  const gameLoop = updater([movementSystem, collisionSystem]);
 
   useAnimationFrame(() => {
     const newWorld = gameLoop({ particles, events: [] });
@@ -71,30 +35,16 @@ const Board = (props: React.PropsWithChildren<IRect>) => {
       }}
     >
       <Ball {...particles[0]} />
-      <Grid x={190} y={150} />
     </svg>
   );
 };
 
 const particleFactory = (): IParticle[] => {
-  const { width, height } = brickSize;
-  const x = 190;
-  const y = 150;
-
-  const bricks = Array.from(Array(rows), (_, row) =>
-    Array.from(Array(cols), (_, col) => ({
-      id: idFactory(),
-      family: "brick",
-      pos: { x: x + width * col, y: y + row * height },
-      size: { width, height },
-    }))
-  );
-
   return [
     {
       id: idFactory(),
       pos: { x: 30, y: 30 },
-      radius: 5,
+      radius: 20,
       velocity: { x: 3, y: 3 },
     },
     {
@@ -117,7 +67,6 @@ const particleFactory = (): IParticle[] => {
       pos: { x: -10, y: 0 },
       size: { width: 10, height: 600 },
     },
-    ...bricks.flat(),
   ];
 };
 
