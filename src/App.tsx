51905f8b6332803b0d814ffaf56d19a3945969f8
Nick Nance <nance.nick@gmail.com>
2020-09-03 07:15:44 -0700

    add bootstrap layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
+  const colRef = React.useRef<HTMLDivElement>(null);
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  React.useEffect(() => {
+    if (colRef.current && canvasRef.current) {
+      canvasRef.current.width = colRef.current.clientWidth;
+      canvasRef.current.height = colRef.current.clientHeight;
+    }
+  }, [colRef, canvasRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container fluid style={{ height: "100vh" }}>
+        <Row style={{ height: "100%" }}>
+          <Col></Col>
+          <Col
+            ref={colRef}
+            md={9}
+            style={{ height: "100%", textAlign: "center" }}
+          >
+            <canvas ref={canvasRef} />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
